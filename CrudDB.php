facf9c3a51b04f948221c19c40a39e3676738822
javagrupo2015 <david-9595@hotmail.com>
2018-05-20 16:52:18 -0500

    first commit

---
+++ CrudDB.php
@@ -0,0 +1,25 @@
+<?php 
+    
+    include('config.php') ;
+    $con = conectar();   
+    echo "se realizo exitosamente la conexion a la base de datos"; 
+
+    class CRUDusuario {
+
+        function  AgregarUsuario(){
+
+
+
+        }   
+
+
+
+    }
+
+
+
+
+
+
+
+?> 
---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+function conectar(){
+
+    $user = "root"; 
+    $pass = ""; 
+    $server="localhost"; 
+    $db = "calculonomina"; 
+    $con = mysqli_connect($server, $user, $pass, $db ) or die ("Error en la conexion"); 
+    return $con; 
+
+}
+
+?>
