c7a22b684a971353d19b2290a5461a73c4d95cf6
javagrupo2015 <david-9595@hotmail.com>
2018-05-20 22:18:08 -0500

    dto's

---
+++ dto/DtoUsuario.php
@@ -0,0 +1,13 @@
+<?php
+
+class dtoUsuario {
+  
+    public $IdUsuario = 0 ;
+    public $NombreCompleto ="";
+    public $Apellido = "";
+    public $Usuario = "";
+    public $Contrasena = "";
+    public $IdTipoCargo = "" ;
+}
+
+?> 
