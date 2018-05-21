b38648792299289fde131664ad8e20eb5530eedd
javagrupo2015 <david-9595@hotmail.com>
2018-05-20 22:17:03 -0500

    avance uso dto's

--- CrudDB.php
+++ CrudDB.php
@@ -1,25 +1,34 @@
 <?php 
     
     include('config.php') ;
-    $con = conectar();   
-    echo "se realizo exitosamente la conexion a la base de datos"; 
+    include('dto/DtoUsuario.php') ;
 
-    class CRUDusuario {
-
-        function  AgregarUsuario(){
-
-
- // hola fabian estoy probando los cambios 
+    //echo "se realizo exitosamente la conexion a la base de datos"; 
+    class CRUDusuario 
+     { 
+        function  AgregarUsuario($dtoUsuarioGuardar){
+            $conec =conectar();  
+            $sql = "INSERT INTO usuario (NombreCompleto,Apellido,usuario,contrasena,IdTipoCargo) 
+                    VALUES ('$dtoUsuarioGuardar->NombreCompleto','$dtoUsuarioGuardar->Apellido',
+                   '$dtoUsuarioGuardar->Usuario', '$dtoUsuarioGuardar->Contrasena',$dtoUsuarioGuardar->IdTipoCargo)"; 
+                    $ejecutar =mysqli_query($conec ,$sql); 
+            if($ejecutar){
+                    echo"bien Guardado"; 
+            }
+            else {
+                echo "No guardo nada"  ; 
+            }
         }   
-
-
-
     }
-
-
-
-
-
-
-
+    
+    $dtoUsuarios = new dtoUsuario; 
+    $dtoUsuarios->NombreCompleto = "nicolas DAvid "; 
+    $dtoUsuarios->Apellido = "Cortes Muñoz "; 
+    $dtoUsuarios->usuario = "christianc"; 
+    $dtoUsuarios->Contrasena = "123"; 
+    $dtoUsuarios->IdTipoCargo = 1 ;
+    echo  $dtoUsuarios->usuario; 
+    
+    $clase = new CRUDusuario; 
+    $clase -> AgregarUsuario($dtoUsuarios); 
 ?> 
